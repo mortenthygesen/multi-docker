3dc067ca6b9cc51d9024fe0c0d5fb55b9fe0e8b0
Morten Thygesen <morten.thygesen@capgeminisogeti.dk>
2019-11-27 18:58:12 +0100

    Changed headline text

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Fib Calculator</h1>
+          <h1 className="App-title">Fib Calculator version 2</h1>
           <Link to="/">Home</Link>
           <Link to="/otherPage">Other Page</Link>
         </header>
